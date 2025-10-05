78b3d2ae1070a2e4bab49573b8ab36f87487d4e3
yiyang-gao <stnvyga@ucl.ac.uk>
2025-10-05 14:07:08 +0100

    Final Check

--- include.js
+++ include.js
@@ -1,5 +1,4 @@
-<!-- include.js -->
-<script>
+// include.js
 (async function () {
   // Inject header/footer
   async function inject(sel, file){
@@ -8,6 +7,7 @@
     const res = await fetch(file, {cache: 'no-store'});
     host.innerHTML = await res.text();
   }
+
   await inject('[data-include="header"]', '/header.html');
   await inject('[data-include="footer"]', '/footer.html');
 
@@ -29,7 +29,7 @@
       // Exact page matches (cv.html, portfolio.html, research.html, publications.html)
       if (hrefPath === path) a.classList.add('active');
 
-      // When we are on index and an anchor is present, allow About/Research/etc. to be active
+      // When on index and an anchor is present, allow About/Research/etc. to be active
       if ((path === '/index.html') && href.startsWith('/index.html#')) {
         if (location.hash && href.endsWith(location.hash)) a.classList.add('active');
       }
@@ -57,7 +57,7 @@
     });
   }
 
-  // If lands with a hash from another page, jump to section
+  // If landing with a hash from another page, jump to section
   function jumpIfHasHash(){
     if (location.hash){
       const id = location.hash.slice(1);
@@ -66,4 +66,3 @@
     }
   }
 })();
-</script>
